a5d63e39b8181681e80bb18f84acae6957ca899d
snqu-ios <qinyuelong.cn@gmail.com>
2016-03-30 18:11:22 +0800

    添加 DrawPad 项目 但是 还未完成

---
+++ DrawPad/DrawPad/ViewController.swift
@@ -0,0 +1,142 @@
+//
+//  ViewController.swift
+//  DrawPad
+//
+//  Created by Jean-Pierre Distler on 13.11.14.
+//  Copyright (c) 2014 Ray Wenderlich. All rights reserved.
+//
+// https://www.raywenderlich.com/87899/make-simple-drawing-app-uikit-swift
+
+
+import UIKit
+import CoreGraphics
+
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var mainImageView: UIImageView!
+    @IBOutlet weak var tempImageView: UIImageView!
+    
+    var lastPoint = CGPoint.zero
+    var red: CGFloat = 0.0
+    var green: CGFloat = 0.0
+    var blue: CGFloat = 0.0
+    var brushWidth: CGFloat = 10.0
+    var opacity: CGFloat = 1.0
+    var swiped = false
+    
+    let colors: [(CGFloat, CGFloat, CGFloat)] = [
+        (0, 0, 0),
+        (105.0 / 255.0, 105.0 / 255.0, 105.0 / 255.0),
+        (1.0, 0, 0),
+        (0, 0, 1.0),
+        (51.0 / 255.0, 204.0 / 255.0, 1.0),
+        (102.0 / 255.0, 204.0 / 255.0, 0),
+        (102.0 / 255.0, 1.0, 0),
+        (160.0 / 255.0, 82.0 / 255.0, 45.0 / 255.0),
+        (1.0, 102.0 / 255.0, 0),
+        (1.0, 1.0, 0),
+        (1.0, 1.0, 1.0),
+    ]
+
+    override func viewDidLoad() {
+      super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+      super.didReceiveMemoryWarning()
+      // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Actions
+
+    @IBAction func reset(sender: AnyObject) {
+        mainImageView.image = nil
+    }
+
+    @IBAction func share(sender: AnyObject) {
+    }
+  
+    @IBAction func pencilPressed(sender: AnyObject) {
+        var index = sender.tag ?? 0
+        if index < 0 || index >= colors.count {
+            index = 0
+        }
+        
+        // 2
+        (red, green, blue) = colors[index]
+        
+        // 3
+        if index == colors.count - 1 {
+            opacity = 1.0
+        }
+        
+    }
+    
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        swiped = false
+        if let touch = touches.first {
+            lastPoint = touch.locationInView(self.view)
+        }
+    }
+    
+    override func touchesMoved(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        // 6
+        swiped = true
+        if let touch = touches.first {
+            let currentPoint = touch.locationInView(view)
+            drawLineFrom(lastPoint, toPoint: currentPoint)
+            
+            // 7 
+            lastPoint = currentPoint
+        }
+    }
+    
+    override func touchesEnded(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        if !swiped {
+            //  没有滑动 就画一个点
+            drawLineFrom(lastPoint, toPoint: lastPoint)
+        }
+        
+        UIGraphicsBeginImageContext(mainImageView.frame.size)
+        mainImageView.image?.drawInRect(CGRectMake(0, 0, view.frame.size.width, view.frame.size.height), blendMode: .Normal, alpha: 1.0)
+        
+        tempImageView.image?.drawInRect(CGRectMake(0, 0, view.frame.size.width, view.frame.size.height), blendMode: .Normal, alpha: opacity)
+        
+        mainImageView.image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        tempImageView.image = nil
+    }
+    
+    
+    func drawLineFrom(fromPoint: CGPoint, toPoint: CGPoint) {
+        // 1
+        UIGraphicsBeginImageContext(view.frame.size)
+        let context = UIGraphicsGetCurrentContext()
+        tempImageView.image?.drawInRect(CGRectMake(0, 0, view.frame.size.width, view.frame.size.height))
+        
+        // 2
+        CGContextMoveToPoint(context, fromPoint.x, fromPoint.y)
+        CGContextAddLineToPoint(context, toPoint.x, toPoint.y)
+        
+        // 3
+        CGContextSetLineCap(context, CGLineCap.Round)
+        CGContextSetLineWidth(context, brushWidth)
+        CGContextSetRGBStrokeColor(context, red, green, blue, 1.0)
+        CGContextSetBlendMode(context, .Normal)
+        
+        // 4
+        CGContextStrokePath(context)
+        
+        
+        // 5 
+        tempImageView.image = UIGraphicsGetImageFromCurrentImageContext()
+        tempImageView.alpha = opacity
+        UIGraphicsEndImageContext()
+        
+    }
+    
+}
+
